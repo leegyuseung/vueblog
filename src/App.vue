617315b274b964ba2886e6bde7d1319a7b4bc552
leegyuseung <lgs4002@naver.com>
2022-10-04 23:22:54 +0900

    bootstrap 사용, 데이터입력

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Vuelog</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link disabled">Disabled</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <h5>React 개발자의 블로그입니다</h5>
+    <p>Vue로 만들었음</p>
+  </div>
+
+  <List :blog="blogs" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import List from "./components/ListBoard.vue";
+import blog from "./assets/blog";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return { blogs: blog };
+  },
+  components: { List },
+};
 </script>
 
 <style>
@@ -21,6 +51,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ListBoard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h4>{{ blog[0].title }}</h4>
+    <p>{{ blog[0].content }}</p>
+    <p>{{ blog[0].date }}</p>
+  </div>
+  <div>
+    <h4>{{ blog[1].title }}</h4>
+    <p>{{ blog[1].content }}</p>
+    <p>{{ blog[1].date }}</p>
+  </div>
+  <div>
+    <h4>{{ blog[2].title }}</h4>
+    <p>{{ blog[2].content }}</p>
+    <p>{{ blog[2].date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListBoard",
+  props: {
+    blog: Array,
+  },
+};
+</script>
+
+<style></style>
